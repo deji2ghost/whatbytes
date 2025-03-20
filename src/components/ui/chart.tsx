cc27de800892f73403ca5658efb85b15e44cbc3b
deji <dejiwilliams9@gmail.com>
2025-03-20 05:51:29 +0100

    fix: ssr rendering

--- src/components/ui/chart.tsx
+++ src/components/ui/chart.tsx
@@ -1,21 +1,25 @@
 "use client";
 
+import dynamic from "next/dynamic";
 import { LineChart, XAxis, Tooltip, Legend, Line } from "recharts";
 
 interface ChartProps {
   data: { name: string; score: number; percentile: number }[];
 }
 
+const DynamicLineChart = dynamic(() => import("recharts").then((mod) => mod.LineChart), { ssr: false });
+
+
 const CustomChart: React.FC<ChartProps> = ({ data }) => {
   return (
     <div className="p-4 rounded-lg">
       
-        <LineChart width={500} height={300} data={data} margin={{ top: 24, right: 24, left: 24, bottom: 24 }}>
+        <DynamicLineChart width={500} height={300} data={data} margin={{ top: 24, right: 24, left: 24, bottom: 24 }}>
           <XAxis dataKey="score" />
           <Tooltip />
           <Legend />
           <Line type="monotone" dataKey="percentile" stroke="#82ca9d" strokeWidth={2} />
-        </LineChart>
+        </DynamicLineChart>
       
     </div>
   );
